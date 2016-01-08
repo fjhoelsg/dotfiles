a617c810a547bb57c7e3b2f47e9bea5d6138b0cd
Jhoel Salas <fjhoelsg@gmail.com>
2016-01-07 23:09:17 -0600

    Add GOPATH environment variable with check

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,6 +19,14 @@ if [ -z "${GIT_USER_EMAIL}" ]; then
   read -p 'Git email: ' GIT_USER_EMAIL
 fi
 
+# Get GOPATH
+if [ -z "${GOPATH}" ]; then
+  read -p 'GOPATH: ' GOPATH 
+fi
+GOPATH=$(echo "${GOPATH}" | sed -e "s@${HOME}@~@g")
+GOPATH=$(echo "${GOPATH}" | sed -e "s/ /\\\ /g")  
+sh -c "mkdir -p ${GOPATH}"
+
 # Pre scripts
 source "${HOOKS_PATH}/before"
 
@@ -28,8 +36,10 @@ do
   cp "${file}" ~/.$(basename "${file}")
 done
 
+# Substitute variables in files
 sed -i.bak "s|\[GIT_USER_NAME\]|$GIT_USER_NAME|g" ~/.gitconfig
 sed -i.bak "s|\[GIT_USER_EMAIL\]|$GIT_USER_EMAIL|g" ~/.gitconfig
+sed -i.bak "s|\[GOPATH\]|$GOPATH|g" ~/.zshenv
 
 # Post scripts
 source "${HOOKS_PATH}/after"
--- zshenv
+++ zshenv
@@ -3,7 +3,7 @@ dsremove () {
   find "${1:-.}" -name '*.DS_Store' -type f -delete
 }
 
-# Exports
+# General Exports
 export EDITOR=vim
 export VISUAL=$EDITOR
 export GIT_EDITOR=$EDITOR
@@ -11,3 +11,9 @@ export LANG='en_US.UTF-8'
 export LC_ALL='en_US.UTF-8'
 export PATH=/usr/local/bin:$PATH
 export PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
+
+# Go
+export GOPATH=[GOPATH]
+if [ -d "${GOPATH}" ]; then
+  export PATH=$PATH:$GOPATH/bin
+fi
