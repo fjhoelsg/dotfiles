24fddf2f50ee9beafe0a4b20cb32d5a259a52f85
Jhoel Salas <fjhoelsg@gmail.com>
2016-01-07 22:01:10 -0600

    Add git user name and email to gitconfig with check

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,18 @@ HOOKS_PATH=$ROOT_DIR/hooks
 FILES=$(find "${ROOT_DIR}" -type f -not -path '*/\.*' -not -name "${FILENAME}" \
   -not -name README.md -maxdepth 1)
 
+# Get git user name
+GIT_USER_NAME=$(git config --global --get user.name)
+if [ -z "${GIT_USER_NAME}" ]; then
+  read -p 'Git name: ' GIT_USER_NAME
+fi
+
+# Get git user email
+GIT_USER_EMAIL=$(git config --global --get user.email)
+if [ -z "${GIT_USER_EMAIL}" ]; then
+  read -p 'Git email: ' GIT_USER_EMAIL
+fi
+
 # Pre scripts
 source "${HOOKS_PATH}/before"
 
@@ -15,6 +27,9 @@ for file in $FILES
 do
   cp "${file}" ~/.$(basename "${file}")
 done
+
+sed -i.bak "s|\[GIT_USER_NAME\]|$GIT_USER_NAME|g" ~/.gitconfig
+sed -i.bak "s|\[GIT_USER_EMAIL\]|$GIT_USER_EMAIL|g" ~/.gitconfig
 
 # Post scripts
 source "${HOOKS_PATH}/after"
